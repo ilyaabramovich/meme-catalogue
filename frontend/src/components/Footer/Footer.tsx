a00195331de06f82d2e5e169d2fafbbca8884f38
IxxyDev <denisenkoforwork@gmail.com>
2022-01-21 00:06:26 +0300

    feat: add header + footer components

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { FooterProps } from './Footer.props';
+
+const Footer: FC<FooterProps> = () => {
+  return (
+    <footer className="">Footer</footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { HeaderProps } from './Header.props';
+
+const Header: FC<HeaderProps> = () => {
+  return (
+    <header className="">Header</header>
+  )
+}
+
+export default Header
